d5f758eff0c2ef662719d1846cd71b3b63f00614
penguninn <coolermaster1082005@gmail.com>
2025-10-11 16:26:44 +0700

    initial base project

---
+++ src/main/java/com/threadcity/jacketshopbackend/JacketShopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.threadcity.jacketshopbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JacketShopBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JacketShopBackendApplication.class, args);
+    }
+
+}
