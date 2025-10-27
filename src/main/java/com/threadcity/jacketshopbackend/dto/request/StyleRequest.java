6a4a7cc8d77cd701d98000e3bf6c44f2a5f45165
lenhatlinh772003 <linhlnph24509@fpt.edu.vn>
2025-10-27 16:55:13 +0700

    feat: tao xong api crud style

---
+++ src/main/java/com/threadcity/jacketshopbackend/dto/request/StyleRequest.java
@@ -0,0 +1,17 @@
+package com.threadcity.jacketshopbackend.dto.request;
+
+import com.threadcity.jacketshopbackend.common.Enums;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.Instant;
+
+@Data
+@Builder
+public class StyleRequest {
+    @NotNull(message = "Name cannot be null")
+    private String name; // bomber, biker, hoodie, blazer, etc.
+    private String description;
+    private Enums.Status status;
+}
