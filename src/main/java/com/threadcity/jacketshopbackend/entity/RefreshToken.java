e3b0085f52b484ede17effea6686312ff2f3e3cc
penguninn <coolermaster1082005@gmail.com>
2025-10-29 22:53:35 +0700

    feat(auth): add entity RefreshToken for rotation function

---
+++ src/main/java/com/threadcity/jacketshopbackend/entity/RefreshToken.java
@@ -0,0 +1,64 @@
+package com.threadcity.jacketshopbackend.entity;
+
+import java.time.Instant;
+
+import org.hibernate.resource.beans.internal.FallbackBeanInstanceProducer;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.security.core.userdetails.User;
+
+import com.threadcity.jacketshopbackend.common.Enums.RefreshTokenStatus;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.Setter;
+
+@Entity
+@Table(name = "auth_refresh_tokens")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(nullable = false, unique = true, length = 64)
+    private String jti;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 16)
+    private RefreshTokenStatus status;
+
+    @Column(nullable = false)
+    private Instant expiresAt;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private Instant createdAt;
+
+    @LastModifiedDate
+    private Instant updatedAt;
+}
