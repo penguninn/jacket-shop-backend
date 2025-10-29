842bd799e060dc9c9b89eb7b572acf1ca7ad4fbb
penguninn <coolermaster1082005@gmail.com>
2025-10-29 22:48:37 +0700

    feat(audit): create new auditing config

---
+++ src/main/java/com/threadcity/jacketshopbackend/configuration/AuditingConfig.java
@@ -0,0 +1,33 @@
+package com.threadcity.jacketshopbackend.configuration;
+
+import java.util.Optional;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+@Configuration
+@EnableJpaAuditing
+public class AuditingConfig {
+
+    @Bean
+    public AuditorAware<String> auditorProvider() {
+        return new AuditorAwareImpl();
+    }
+
+    public static class AuditorAwareImpl implements AuditorAware<String> {
+
+        @Override
+        public Optional<String> getCurrentAuditor() {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            if (authentication == null || !authentication.isAuthenticated()) {
+                return Optional.of("SYSTEM");
+            }
+            return Optional.ofNullable(authentication.getName());
+        }
+
+    }
+}
