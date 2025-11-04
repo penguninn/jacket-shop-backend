c8c2f49ab4373332acf10d508bafb86cceae90ab
St4ck1n <dlvad3169@gmail.com>
2025-11-04 21:53:25 +0700

    Tao xong api/coupons

--- src/main/java/com/threadcity/jacketshopbackend/dto/response/CouponResponse.java
+++ src/main/java/com/threadcity/jacketshopbackend/dto/response/CouponResponse.java
@@ -5,16 +5,24 @@ import lombok.Builder;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.time.Instant;
 
 @Data
 @Builder
-public class SizeResponse implements Serializable {
+public class CouponResponse implements Serializable {
     private Long id;
-    private String name;
+    private String code;
     private String description;
+    private Enums.CouponType type;
+    private BigDecimal value;
+    private BigDecimal minOrderValue;
+    private BigDecimal maxDiscount;
+    private Integer usageLimit;
+    private Integer usedCount;
+    private Instant validFrom;
+    private Instant validTo;
     private Enums.Status status;
     private Instant createdAt;
     private Instant updatedAt;
-
 }
