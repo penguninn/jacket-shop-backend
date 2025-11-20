c2535efa31194ede7a4ff5bf304f9529564c731d
duchuyneh <duchuy22xgmail.com>
2025-11-20 10:39:59 +0700

    sua material

--- src/main/java/com/threadcity/jacketshopbackend/service/MaterialService.java
+++ src/main/java/com/threadcity/jacketshopbackend/service/MaterialService.java
@@ -1,5 +1,6 @@
 package com.threadcity.jacketshopbackend.service;
 
+import com.threadcity.jacketshopbackend.dto.request.MaterialFilterRequest;
 import com.threadcity.jacketshopbackend.dto.request.MaterialRequest;
 import com.threadcity.jacketshopbackend.dto.response.MaterialResponse;
 import com.threadcity.jacketshopbackend.dto.response.PageResponse;
@@ -7,14 +8,15 @@ import com.threadcity.jacketshopbackend.entity.Material;
 import com.threadcity.jacketshopbackend.exception.BusinessException;
 import com.threadcity.jacketshopbackend.mapper.MaterialMapper;
 import com.threadcity.jacketshopbackend.repository.MaterialRepository;
+import com.threadcity.jacketshopbackend.specification.MaterialSpecification;
+
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
+
+import org.springframework.data.domain.*;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -23,86 +25,104 @@ import java.util.List;
 @RequiredArgsConstructor
 @Slf4j
 public class MaterialService {
+
     private final MaterialRepository materialRepository;
     private final MaterialMapper materialMapper;
 
-    public MaterialResponse getMaterialById(Long Id) {
-        log.info("MaterialService::getMaterialById - Execution started. [Id: {}]", Id);
-        Material material = materialRepository.findById(Id)
-                .orElseThrow(() -> new EntityNotFoundException("Material not found with MaterialId: " + Id));
-        log.info("MaterialService::getMaterialById - Execution completed. [MaterialId: {}]", Id);
+    public MaterialResponse getMaterialById(Long id) {
+        log.info("MaterialService::getMaterialById - Execution started. [Id: {}]", id);
+        Material material = materialRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Material not found with MaterialId: " + id));
+        log.info("MaterialService::getMaterialById - Execution completed. [MaterialId: {}]", id);
         return materialMapper.toDto(material);
     }
 
-    public PageResponse<?> getAllMaterials(int page, int size, String sortBy) {
-        log.info("MaterialService::getAllMaterial - Execution started.");
+    public PageResponse<?> getAllMaterials(MaterialFilterRequest request) {
+        log.info("MaterialService::getAllMaterials - Execution started.");
+
         try {
-            int p = Math.max(0, page);
-            String[] sortParams = sortBy.split(",");
-            Sort sortOrder = Sort.by(Sort.Direction.fromString(sortParams[1]), sortParams[0]);
-            Pageable pageable = PageRequest.of(p, size, sortOrder);
-            Page<Material> materialPage = materialRepository.findAll(pageable);
-            List<MaterialResponse> MaterialList = materialPage.stream()
+            Sort sort = Sort.by(Sort.Direction.fromString(request.getSortDir()), request.getSortBy());
+            Pageable pageable = PageRequest.of(request.getPage(), request.getSize(), sort);
+
+            Specification<Material> spec = MaterialSpecification.buildSpec(request);
+            Page<Material> materialPage = materialRepository.findAll(spec, pageable);
+
+            List<MaterialResponse> materialList = materialPage.getContent().stream()
                     .map(materialMapper::toDto)
                     .toList();
-            log.info("MaterialService::getAllMaterial - Execution completed.");
+
+            log.info("MaterialService::getAllMaterials - Execution completed.");
             return PageResponse.builder()
-                    .contents(MaterialList)
-                    .size(size)
-                    .page(p)
+                    .contents(materialList)
+                    .size(request.getSize())
+                    .page(request.getPage())
                     .totalPages(materialPage.getTotalPages())
-                    .totalElements(materialPage.getTotalElements()).build();
+                    .totalElements(materialPage.getTotalElements())
+                    .build();
+
         } catch (Exception e) {
-            log.error("MaterialService::getAllMaterial - Execution failed.", e);
-            throw new BusinessException("MaterialService::getAllMaterial - Execution failed.");
+            log.error("MaterialService::getAllMaterials - Execution failed.", e);
+            throw new BusinessException("MaterialService::getAllMaterials - Execution failed.");
         }
     }
+
     @Transactional
-    public MaterialResponse createMaterial(MaterialRequest material) {
+    public MaterialResponse createMaterial(MaterialRequest request) {
         log.info("MaterialService::createMaterial - Execution started.");
-        if (materialRepository.existsByName(material.getName())) {
-            throw new BusinessException("material already exists with name: " + material.getName());
+
+        if (materialRepository.existsByName(request.getName())) {
+            throw new BusinessException("Material already exists with name: " + request.getName());
         }
+
         try {
-            Material materialEntity = materialMapper.toEntity(material);
-            Material savedMaterial = materialRepository.save(materialEntity);
+            Material material = materialMapper.toEntity(request);
+            Material savedMaterial = materialRepository.save(material);
+
             log.info("MaterialService::createMaterial - Execution completed.");
             return materialMapper.toDto(savedMaterial);
+
         } catch (Exception e) {
             log.error("MaterialService::createMaterial - Execution failed.", e);
             throw new BusinessException("MaterialService::createMaterial - Execution failed.");
         }
     }
+
     @Transactional
-    public MaterialResponse updateMaterialById(MaterialRequest materialRequest,Long id) {
+    public MaterialResponse updateMaterialById(MaterialRequest request, Long id) {
         log.info("MaterialService::updateMaterialById - Execution started.");
+
+        Material material = materialRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Material not found with MaterialId: " + id));
+
         try {
-            Material material = materialRepository.findById(id).orElseThrow(() ->
-                    new EntityNotFoundException("Material not found with MaterialId: " + id));
-            material.setName(materialRequest.getName());
-            material.setDescription(materialRequest.getDescription());
-            material.setStatus(materialRequest.getStatus());
+            material.setName(request.getName());
+            material.setDescription(request.getDescription());
+            material.setStatus(request.getStatus());
+
             Material savedMaterial = materialRepository.save(material);
-            log.info("MaterialService::updateProfile - Execution completed. [MaterialId: {}]", id);
+            log.info("MaterialService::updateMaterialById - Execution completed. [MaterialId: {}]", id);
             return materialMapper.toDto(savedMaterial);
+
         } catch (RuntimeException e) {
-            log.error("MaterialService::updateProfile - Execution failed.", e);
-            throw new BusinessException("MaterialService::updateProfile - Execution failed.");
+            log.error("MaterialService::updateMaterialById - Execution failed.", e);
+            throw new BusinessException("MaterialService::updateMaterialById - Execution failed.");
         }
     }
 
     @Transactional
     public void deleteMaterial(Long id) {
-        log.info("MaterialService::deleteMaterial - Execution started.");
+        log.info("MaterialService::deleteMaterial - Execution started. [MaterialId: {}]", id);
+
+        if (!materialRepository.existsById(id)) {
+            throw new EntityNotFoundException("Material not found with MaterialId: " + id);
+        }
+
         try {
-            if (!materialRepository.existsById(id)) {
-                throw new EntityNotFoundException("Material not found with MaterialId: " + id);
-            }
             materialRepository.deleteById(id);
-            log.info("MaterialService::deleteMaterial - Execution completed.");
+            log.info("MaterialService::deleteMaterial - Execution completed. [MaterialId: {}]", id);
         } catch (Exception e) {
-            log.error("MaterialService::deleteMaterial - Execution failed.");
-            throw new BusinessException("MaterialService::deleteMaterial - Execution failed." );
+            log.error("MaterialService::deleteMaterial - Execution failed.", e);
+            throw new BusinessException("MaterialService::deleteMaterial - Execution failed.");
         }
     }
 }
