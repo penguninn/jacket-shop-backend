8b08c1a56162a4b0840bd84807e7dae7c1fd8e18
St4ck1n <dlvad3169@gmail.com>
2025-11-21 10:30:31 +0700

    sua color

---
+++ src/main/java/com/threadcity/jacketshopbackend/specification/ColorSpecification.java
@@ -0,0 +1,45 @@
+package com.threadcity.jacketshopbackend.specification;
+
+
+
+import com.threadcity.jacketshopbackend.dto.request.ColorFilterRequest;
+import com.threadcity.jacketshopbackend.entity.Color;
+import com.threadcity.jacketshopbackend.common.Enums.Status;
+
+import org.springframework.data.jpa.domain.Specification;
+
+import java.util.List;
+
+public class ColorSpecification {
+
+    public static Specification<Color> buildSpec(ColorFilterRequest request) {
+        return Specification.where(search(request.getSearch()))
+                .and(filterStatus(request.getStatus()));
+    }
+
+    private static Specification<Color> search(String search) {
+        return (root, query, cb) -> {
+            if (search == null || search.isBlank()) {
+                return null;
+            }
+            String pattern = "%" + search.toLowerCase() + "%";
+            return cb.or(
+                    cb.like(cb.lower(root.get("name")), pattern),
+                    cb.like(cb.lower(root.get("description")), pattern)
+            );
+        };
+    }
+
+    private static Specification<Color> filterStatus(List<String> statuses) {
+        return (root, query, cb) -> {
+            if (statuses == null || statuses.isEmpty()) {
+                return null;
+            }
+            List<Status> statusEnums = statuses.stream()
+                    .map(s -> Status.valueOf(s.toUpperCase()))
+                    .toList();
+
+            return root.get("status").in(statusEnums);
+        };
+    }
+}
