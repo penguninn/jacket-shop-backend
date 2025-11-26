297402a581f1883cac4da442b080bcac454a0f79
duchuyneh <duchuy22xgmail.com>
2025-11-26 14:40:08 +0700

    crud payment

---
+++ src/main/java/com/threadcity/jacketshopbackend/dto/response/PaymentMethodResponse.java
@@ -0,0 +1,27 @@
+package com.threadcity.jacketshopbackend.dto.response;
+
+import com.threadcity.jacketshopbackend.common.Enums;
+import lombok.Builder;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+@Data
+@Builder
+public class PaymentMethodResponse implements Serializable {
+
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private String configJson;
+
+    private Enums.Status status;
+
+    private Instant createdAt;
+
+    private Instant updatedAt;
+}
